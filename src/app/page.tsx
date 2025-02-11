b12e0beb536a1cdb0a6ed46aea97d7d1d1dffcfb
higunito <reisen0615akira@gmail.com>
2025-02-12 02:01:48 +0900

    setup nextjs and tailwind v4

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+// import Image from "next/image";
+// import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-center text-blue-500">
+        Hello, Next.js and TailwindCSS v4!!!
+      </h1>
+    </div>
+  );
+}
